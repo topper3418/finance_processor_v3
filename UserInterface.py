5cfb8893d7966a91f43105602e1306b19b4f862e
topper3418 <67675579+topper3418@users.noreply.github.com>
2022-12-15 20:42:03 -0800

    gonna continue upstairs

--- UserInterface.py
+++ UserInterface.py
@@ -1,7 +1,8 @@
 import os
+from typing import Dict, List
+from PySheets import from_csv
 
 
-# prompt user for name (1: travis, 2: emily)
 def get_name():
     name_ind = input('Available profiles:\n'
                      '1) Travis\n'
@@ -14,8 +15,7 @@ def get_name():
     return ['Travis', 'Emily'][int(name_ind) - 1]
 
 
-# prompt user for filepath to directory
-def get_filepath(user):
+def get_filepaths(user):
     filepath = input(f"Hello, {user}.\n"
                      f"\n"
                      f"Please enter the filepath containing\n"
@@ -24,22 +24,39 @@ def get_filepath(user):
                      f"filepath: ")
     if not os.path.isdir(filepath):
         raise Exception('Error, filepath not found')
-    print(os.listdir(filepath))
+    if not any(file.endswith('.csv') for file in os.listdir(filepath)):
+        raise Exception('Error, no csv files found in directory')
+    print('\n\n\n')
+    return filepath
 
 
-# loop through files, loading into objects
-# load all into an "all_raw_table", headers [date, amount, memo, account, person]
-# do a first pass, trying to match vendors and types based on memos from DB
-# find first (chronological) unknown type or vendor
-# prompt user to copy "snippet" to identify vendor
-# look vendor up in vendor database [vendor key, vendor id, typical type]
-##  if new vendor, ask for type (repeat lookup process), add new vendor
-##  if existing vendor, suggest the most common type, allow for override
-# add those values to the table
+def get_raw_objects(filepath):
+    os.chdir(filepath)
+    dict_out: Dict[str: List[list]] = {}
+    for file in os.listdir(filepath):
+        if not file.endswith('.csv'):
+            print(f'skipping {file}')
+            continue
+        print(f'loading {file}')
+        dict_out[file] = from_csv(file)
+    return dict_out
+
 
 def main():
+    # prompt user for name (1: travis, 2: emily)
     user = get_name()
-    get_filepath(user)
+    # prompt user for filepath to directory
+    filepath = get_filepaths(user)
+    # loop through files, loading into objects
+    raw_data = get_raw_objects(filepath)
+    # load all into an "all_raw_table", headers [date, amount, memo, account, person]
+    # do a first pass, trying to match vendors and types based on memos from DB
+    # find first (chronological) unknown type or vendor
+    # prompt user to copy "snippet" to identify vendor
+    # look vendor up in vendor database [vendor key, vendor id, typical type]
+    ##  if new vendor, ask for type (repeat lookup process), add new vendor
+    ##  if existing vendor, suggest the most common type, allow for override
+    # add those values to the table
 
 
 if __name__ == '__main__':
